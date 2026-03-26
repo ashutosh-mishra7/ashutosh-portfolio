2c2a034c12cc2eb85978f4018da58919dccd0532
ashutosh-mishra7 <mishraashutosh9199@gmail.com>
2026-03-26 23:47:06 +0530

    updated website

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,37 +1,38 @@
 export const certificatesData = [
   {
     id: 1,
+    title: "Claude Code",
+    issuer: "Anthropic",
+    date: "2026",
+    image: "/certificates/CC.png",
+  },
+  {
+    id: 2,
     title: "Machine Learning",
     issuer: "Microsoft",
     date: "2025",
     image: "/certificates/ML.png",
   },
   {
-    id: 2,
+    id: 3,
     title: "SIH Participation",
     issuer: "Shri Ramswaroop Memorial University",
     date: "2025",
     image: "/certificates/Sih.png",
   },
   {
-    id: 3,
+    id: 4,
     title: "Introduction AI",
     issuer: "Great Learning",
     date: "2024",
     image: "/certificates/GL.png",
   },
   {
-    id: 4,
+    id: 5,
     title: "javaScript",
     issuer: "Namaste Dev",
     date: "2024",
     image: "/certificates/JS.jpeg",
   },
-  {
-    id: 5,
-    title: "Claude Code",
-    issuer: "Anthropic",
-    date: "2026",
-    image: "/certificates/CC.png",
-  },
+  
 ]
